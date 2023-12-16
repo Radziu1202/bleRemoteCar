835847bfbcbad76593a45c5b287505b2d71ab565
Radziu1202 <bkamil0320@gmail.com>
2023-12-16 17:40:37 +0100

    modified buttons

--- app/src/main/java/com/punchthrough/blestarterappandroid/ButtonActivity.kt
+++ app/src/main/java/com/punchthrough/blestarterappandroid/ButtonActivity.kt
@@ -18,6 +18,8 @@ class ButtonActivity : AppCompatActivity() {
         setContentView(R.layout.activity_button)
         val rightButton: Button = findViewById(R.id.right_button)
         val leftButton: Button = findViewById(R.id.left_button)
+        val forwardButton: Button = findViewById(R.id.forward_button)
+        val backwardButton: Button = findViewById(R.id.backward_button)
         // Retrieve the BluetoothCharacteristic from the Intent
         val characteristic = intent.getParcelableExtra<BluetoothGattCharacteristic>("characteristic_key")
         val device = intent.getParcelableExtra<BluetoothDevice>("device")
@@ -58,6 +60,44 @@ class ButtonActivity : AppCompatActivity() {
                     return false
                 }
             })
+
+
+        forwardButton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        // Handle button press
+                        onForwardButtonPressed(characteristic,device)
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        // Handle button release
+                        onForwardButtonReleased(characteristic,device)
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+
+        backwardButton.setOnTouchListener(object : View.OnTouchListener {
+            override fun onTouch(view: View, event: MotionEvent): Boolean {
+                when (event.action) {
+                    MotionEvent.ACTION_DOWN -> {
+                        // Handle button press
+                        onBackwardButtonPressed(characteristic,device)
+                        return true
+                    }
+                    MotionEvent.ACTION_UP -> {
+                        // Handle button release
+                        onBackwardButtonReleased(characteristic,device)
+                        return true
+                    }
+                }
+                return false
+            }
+        })
+
         }
 
         private fun onRightButtonPressed(characteristic: BluetoothGattCharacteristic,device:BluetoothDevice) {
@@ -75,6 +115,23 @@ class ButtonActivity : AppCompatActivity() {
         private fun onLeftButtonReleased(characteristic: BluetoothGattCharacteristic,device:BluetoothDevice) {
             ConnectionManager.writeCharacteristic(device, characteristic, "lewo stop".toByteArray());
         }
+
+
+    private fun onForwardButtonPressed(characteristic: BluetoothGattCharacteristic,device:BluetoothDevice) {
+        ConnectionManager.writeCharacteristic(device, characteristic, "przód start".toByteArray());
+    }
+
+    private fun onForwardButtonReleased(characteristic: BluetoothGattCharacteristic,device:BluetoothDevice) {
+        ConnectionManager.writeCharacteristic(device, characteristic, "przód stop".toByteArray());
+    }
+
+    private fun onBackwardButtonPressed(characteristic: BluetoothGattCharacteristic,device:BluetoothDevice) {
+        ConnectionManager.writeCharacteristic(device, characteristic, "tył start".toByteArray());
+    }
+
+    private fun onBackwardButtonReleased(characteristic: BluetoothGattCharacteristic,device:BluetoothDevice) {
+        ConnectionManager.writeCharacteristic(device, characteristic, "tył stop".toByteArray());
+    }
 
 
             // Do something with the characteristic
